166a93a355924a6952118af3427c49520f149e34
Renato Groffe <renato.groffe@yahoo.com.br>
2019-12-17 20:22:36 -0300

    Teste - Build e Deployment automatizado

--- APIContagem/Controllers/ContadorController.cs
+++ APIContagem/Controllers/ContadorController.cs
@@ -23,7 +23,7 @@ namespace APIContagem.Controllers
                 {
                     _CONTADOR.ValorAtual,
                     Environment.MachineName,
-                    Local = "Container Docker",
+                    Local = " Alteração no valor da propriedade Local - Deployment Automatizado",
                     Sistema = Environment.OSVersion.VersionString,
                     Variavel = configuration["TesteAmbiente"],
                     TargetFramework = Assembly
