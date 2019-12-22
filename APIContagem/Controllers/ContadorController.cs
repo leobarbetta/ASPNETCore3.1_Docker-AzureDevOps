dbb9a2410f1fe95ea33fac8aafa11d82b78ab63d
Renato Groffe <renato.groffe@yahoo.com.br>
2019-12-22 14:03:28 -0300

    Teste - Azure Web App for Containers - Deployment Automatizado

--- APIContagem/Controllers/ContadorController.cs
+++ APIContagem/Controllers/ContadorController.cs
@@ -23,7 +23,7 @@ namespace APIContagem.Controllers
                 {
                     _CONTADOR.ValorAtual,
                     Environment.MachineName,
-                    Local = "Azure Web App for Containers - Deployment Automatizado",
+                    Local = "Azure Web App for Containers - Deployment Automatizado...",
                     Sistema = Environment.OSVersion.VersionString,
                     Variavel = configuration["TesteAmbiente"],
                     TargetFramework = Assembly
