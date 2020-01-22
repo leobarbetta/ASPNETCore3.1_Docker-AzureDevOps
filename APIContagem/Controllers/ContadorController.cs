1342f7d5bd3d27ccfda8be7397431e20a5d10b42
Renato Groffe <renato.groffe@yahoo.com.br>
2020-01-21 22:13:39 -0300

    Update ContadorController.cs

--- APIContagem/Controllers/ContadorController.cs
+++ APIContagem/Controllers/ContadorController.cs
@@ -23,7 +23,7 @@ namespace APIContagem.Controllers
                 {
                     _CONTADOR.ValorAtual,
                     Environment.MachineName,
-                    Local = "Universidade Mackenzie",
+                    Local = "Universidade Mackenzie...",
                     Sistema = Environment.OSVersion.VersionString,
                     Variavel = configuration["TesteAmbiente"],
                     TargetFramework = Assembly
